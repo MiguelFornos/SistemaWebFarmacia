2ca513571655a25c0f3addb2ab5fd6b5baf62f43
JulioSanchez <julio239995@gmail.com>
2022-05-07 21:50:35 -0600

    error en el proyecto web, falta el dbContext :(... investigando

--- SisFar.Datos/Model/dbContext.cs
+++ SisFar.Datos/Model/dbContext.cs
@@ -12,9 +12,19 @@ namespace SisFar.Datos.Model
         public dbContext(DbContextOptions<dbContext> options)
            : base(options)
         {
-           
+
         }
 
         public virtual DbSet<Cliente> Clientes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=DESKTOP-4S15HH7\\JULIO;Database=FarmmaciaDB2022;Trusted_Connection=True;");
+
+            }
+        }
     }
 }
